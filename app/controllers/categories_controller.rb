d4d605bbf6936e0946a63b633fbe1a678ce206f5
AvelDanielPadilla <aveldanieln1996@gmail.comgit config --global user.name AvelDanielPadillagit config --global user.email aveldanieln1996@gmail.com>
2025-09-13 18:02:25 +0800

    added category controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,44 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.order(:id)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash.notice = "New category has been added"
+      redirect_to users_path
+    else
+      flash.alert = "Category was not saved"
+      render :new
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(user_params)
+      flash.notic = "Category has ben successfully updated"
+      redirec_to users_path
+    else
+      reder :edit
+    end
+  end
+
+  def delete
+    @category
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:note, :description, :number)
+  end
+end
