355390b824f4b6271429985be2b5fe67dab3c66c
AvelDanielPadilla <aveldanieln1996@gmail.comgit config --global user.name AvelDanielPadillagit config --global user.email aveldanieln1996@gmail.com>
2025-09-13 18:04:58 +0800

    added routes adding category routing

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,15 @@ Rails.application.routes.draw do
 
   post "/users/sign_in" => "users#sign_in", as: "sign_in_user"
 
+  get '/categories' => "categories#index"
+  get "/categories/new" => "categories#new", as: "new_category"
+  post "/categories" => "categories#create", as: "create_category"
+  get "/categories/:id" => "categories#show", as: "category"
+  get "/categories/:id/edit" => "categories#edit", as: "edit_category"
+  put "categories/:id" => "categories#update", as: "update_category"
+  patch "categories/:id" => "categories#update"
+  delete "categories/:id" => "categories#delete", as: "delete_category"
+
   # get "sign_up", "user#new"
   # post "sign_up", to "user#create"
 
